1831c1b895ac0d4dbe0c79879ef0d4d11913cb7c
alvarojg20 <100343573+alvarojg20@users.noreply.github.com>
2022-03-09 10:56:11 +0100

    Delete cursoFebrero/src/main/scala directory

--- cursoFebrero/src/main/scala/Spark.scala
+++
@@ -1,8 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-object Spark {
-  def createLocalSession = {
-    val sparkSession = SparkSession.builder().master(master = "local[*]").appName(name = "curso").config("spark.some.config.option", "some-value").getOrCreate()
-    sparkSession
-  }
-}
