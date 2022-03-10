79998d33db0d048c5c9cb9dbe3f375376c366e18
alvarojg20 <100343573+alvarojg20@users.noreply.github.com>
2022-03-10 09:41:12 +0100

    Add files via upload

--- cursoFebrero/src/main/scala/EjerciciosSparkLearning/Ejercicio2.scala
+++ cursoFebrero/src/main/scala/EjerciciosSparkLearning/Ejercicio2.scala
@@ -10,6 +10,7 @@ object Ejercicio2 extends App{
   val df = sparkSession.read.option("header", "true").option("inferSchema", "true").csv("src/main/resources/EjerciciosSparkLearning/sf-fire-calls.csv")
   df.show()
 
+   //What were all the different types of fire calls in 2018?
   val types18 = df.select(col("CallType"), col("CallDate"))
     .withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
     .filter(year(col("CallDate")).equalTo("2018"))
@@ -17,6 +18,7 @@ object Ejercicio2 extends App{
 
   types18.show()
 
+  //What months within the year 2018 saw the highest number of fire calls?
   val mostCalls18 = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
     .withColumn("Mes", month(col("CallDate")))
     .filter(year(col("CallDate")).equalTo("2018"))
@@ -26,6 +28,7 @@ object Ejercicio2 extends App{
 
   mostCalls18.show()
 
+  //Which neighborhood in San Francisco generated the most fire calls in 2018?
   val neighBour = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
     .filter(year(col("CallDate")).equalTo("2018"))
     .select(col("Neighborhood"))
@@ -34,6 +37,7 @@ object Ejercicio2 extends App{
     .orderBy(desc("count"))
   neighBour.show()
 
+  //Which neighborhoods had the worst response times to fire calls in 2018?
   val delNeigh = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
     .filter(year(col("CallDate")).equalTo("2018"))
     .groupBy(col("Neighborhood")).avg("Delay")
@@ -45,9 +49,14 @@ object Ejercicio2 extends App{
   val dfIOT = sparkSession.read.option("header", "false").json("src/main/resources/EjerciciosSparkLearning/iot_devices.json")
   dfIOT.show()
 
+  //Detect failing devices with battery levels below a threshold.
   val umbral = dfIOT.filter(col("battery_level") < 5)
   umbral.show()
 
+  //Identify offending countries with high levels of CO2 emissions.
   val co2 = dfIOT.select("c02_level", "cn").orderBy(desc("c02_level"))
   co2.show()
+
+  //Compute the min and max values for temperature, battery level, CO2, and humidity.
+  //Sort and group by average temperature, CO2, humidity, and country.
 }
