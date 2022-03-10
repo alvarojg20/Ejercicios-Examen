826ef757ce076c096acde13b0eaedb3fc0f8bf55
alvarojg20 <100343573+alvarojg20@users.noreply.github.com>
2022-03-10 09:34:52 +0100

    Add files via upload

--- cursoFebrero/src/main/scala/EjerciciosSparkLearning/Ejercicio2.scala
+++ cursoFebrero/src/main/scala/EjerciciosSparkLearning/Ejercicio2.scala
@@ -13,25 +13,31 @@ object Ejercicio2 extends App{
   val types18 = df.select(col("CallType"), col("CallDate"))
     .withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
     .filter(year(col("CallDate")).equalTo("2018"))
-    .orderBy(desc("CallDate"))
+    .orderBy(desc("CallDate")).distinct()
 
   types18.show()
 
-  val mostCalls18 = df.select(col("CallDate"))
-    .withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
-    .groupBy(col("CallDate")).count()
+  val mostCalls18 = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
+    .withColumn("Mes", month(col("CallDate")))
     .filter(year(col("CallDate")).equalTo("2018"))
-    .orderBy(desc("CallDate"))
+    .groupBy(col("Mes")).count()
+    .select(col("Mes"), col("count").as("NumFireCalls"))
+    .orderBy(desc("NumFireCalls"))
+
   mostCalls18.show()
 
-  val neighBour = df.select(col("Neighborhood"))
+  val neighBour = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
+    .filter(year(col("CallDate")).equalTo("2018"))
+    .select(col("Neighborhood"))
     .where(col("City").equalTo("SF"))
     .groupBy(col("Neighborhood")).count()
     .orderBy(desc("count"))
   neighBour.show()
 
-  val delNeigh = df.select(col("Neighborhood"), col("Delay"))
-    .orderBy(desc("Delay"))
+  val delNeigh = df.withColumn("CallDate",to_timestamp(col("CallDate"),"MM/dd/yy"))
+    .filter(year(col("CallDate")).equalTo("2018"))
+    .groupBy(col("Neighborhood")).avg("Delay")
+    .orderBy(desc("avg(Delay)"))
   delNeigh.show()
 
 
